1d38de6d0bc319c28588139e5d5256a8b09e5afe
azyui012411 <azyui012411@gmail.com>
2021-03-04 17:03:52 +0800

    add member

---
+++ src/main/webapp/WEB-INF/webView/GBG/js/signup_main.js
@@ -0,0 +1,29 @@
+ $(function (){
+ 
+    function format_float(num, pos)
+    {
+        var size = Math.pow(10, pos);
+        return Math.round(num * size) / size;
+    }
+ 
+    function preview(input) {
+ 
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('.preview').attr('src', e.target.result);
+                var KB = format_float(e.total / 1024, 2);
+                $('.size').text("檔案大小：" + KB + " KB");
+            }
+ 
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+ 
+    $("body").on("change", ".upl", function (){
+        preview(this);
+    })
+    
+})
+ 
