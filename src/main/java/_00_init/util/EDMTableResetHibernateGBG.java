7a11303a97c677d528fa4c70ace9ec455ac4bd50
kooty32142 <kooty32142@gmail.com>
2021-03-25 10:55:00 +0800

    新增order、orderDetail假資料

--- src/main/java/_00_init/util/EDMTableResetHibernateGBG.java
+++ src/main/java/_00_init/util/EDMTableResetHibernateGBG.java
@@ -11,6 +11,7 @@ import java.io.InputStreamReader;
 import java.sql.Blob;
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.hibernate.Hibernate;
@@ -18,14 +19,16 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import com.web.GBG_project.member.model.ManageStatusBean;
+import com.web.GBG_project.member.model.MemberBean;
 import com.web.GBG_project.product.model.CustomerCategoryBean;
 import com.web.GBG_project.product.model.ProductBean;
 import com.web.GBG_project.product.model.ProductCategoryBean;
 import com.web.GBG_project.product.model.ProductDetailBean;
 import com.web.GBG_project.product.model.ProductPicBean;
 import com.web.GBG_project.product.model.ProductStausBean;
+import com.web.GBG_project.shoppingCart.model.OrderDetailsBean;
 import com.web.GBG_project.shoppingCart.model.OrderSatusBean;
+import com.web.GBG_project.shoppingCart.model.OrdersBean;
 
 public class EDMTableResetHibernateGBG {
 	public static final String UTF8_BOM = "\uFEFF"; // 定義 UTF-8的BOM字元
@@ -39,6 +42,7 @@ public class EDMTableResetHibernateGBG {
 		List<ProductCategoryBean> productCategoryList = new ArrayList<>();
 		List<ProductStausBean> productStausList = new ArrayList<>();
 		List<ProductBean> productList = new ArrayList<>();
+		List<OrdersBean> ordersList = new ArrayList<>();
 
 		int count = 0;
 		SessionFactory factory = HibernateUtils.getSessionFactory();
@@ -65,6 +69,71 @@ public class EDMTableResetHibernateGBG {
 //					tx.rollback();
 //				}
 //				System.out.println("orderStList==>" + orderStList);
+		// -------------讀取order資料，寫入資料庫----------------
+		try (
+				// data2/order_satus.dat存放要新增的n筆資料
+				InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "order.dat"), "UTF-8");
+				BufferedReader br = new BufferedReader(isr0);) {
+			tx = session.beginTransaction();
+			while ((line = br.readLine()) != null) {
+				String[] sa = line.split("\\|");
+				OrdersBean order = new OrdersBean();
+				order.setOseq_id(null);
+				order.setOrder_id(sa[0]);
+				MemberBean memberBean = session.get(MemberBean.class, Integer.parseInt(sa[1].trim()));
+				order.setMemberBean(memberBean);
+				order.setReceive_men(sa[2]);
+				order.setShipping_address(sa[3]);
+				Date date = new Date(System.currentTimeMillis());  //之後再測試寫時間
+				order.setOrder_date(date);
+				order.setShipping_style(sa[5]);
+
+				session.save(order);
+				ordersList.add(order);
+
+				count++;
+				System.out.println("新增" + count + "筆記錄:" + sa[0]);
+			}
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			tx.rollback();
+		}
+		System.out.println("ordersList==>" + ordersList);
+		// -------------讀取order_details資料，寫入資料庫----------------
+		try (InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "order_details.dat"), "UTF-8");
+				BufferedReader br = new BufferedReader(isr0);) {
+			while ((line = br.readLine()) != null) {
+				// 未處理BOM字元，若有需要，請自行加入
+				String[] sa = line.split("\\|");
+				try {
+					tx = session.beginTransaction();
+					OrderDetailsBean orderDetail=new OrderDetailsBean();
+					orderDetail.setOdseq_id(null);
+					orderDetail.setOrder_amount(Integer.parseInt(sa[0]));
+					OrdersBean order=session.get(OrdersBean.class, Integer.parseInt(sa[1].trim()));
+					orderDetail.setOrdersBean(order);
+					ProductDetailBean productDetail=session.get(ProductDetailBean.class, Integer.parseInt(sa[2].trim()));
+					orderDetail.setProductDetailBean(productDetail);
+
+					session.save(orderDetail);
+					session.flush();
+					tx.commit();
+					count++;
+					System.out.println("新增" + count + "筆記錄:" + sa[1]);
+					// break;
+				} catch (Exception e) {
+					e.printStackTrace();
+					if (tx != null) {
+						tx.rollback();
+					}
+				} finally {
+
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		// -------------讀取order_satus資料，寫入資料庫----------------
 		try (
@@ -117,10 +186,8 @@ public class EDMTableResetHibernateGBG {
 		System.out.println("customerCategoryList==>" + customerCategoryList);
 
 		// -------------讀取product_Category資料，寫入資料庫----------------
-		try (
-				InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "product_Category.dat"),
-						"UTF-8");
-				BufferedReader br = new BufferedReader(isr0);) {
+		try (InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "product_Category.dat"),
+				"UTF-8"); BufferedReader br = new BufferedReader(isr0);) {
 			tx = session.beginTransaction();
 			while ((line = br.readLine()) != null) {
 				String[] sa = line.split("\\|");
@@ -140,9 +207,7 @@ public class EDMTableResetHibernateGBG {
 		System.out.println("customerCategoryList==>" + customerCategoryList);
 
 		// -------------讀取product_status資料，寫入資料庫----------------
-		try (
-				InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "product_status.dat"),
-						"UTF-8");
+		try (InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "product_status.dat"), "UTF-8");
 				BufferedReader br = new BufferedReader(isr0);) {
 			tx = session.beginTransaction();
 			while ((line = br.readLine()) != null) {
@@ -227,8 +292,7 @@ public class EDMTableResetHibernateGBG {
 
 		// -------------讀取Product_detail資料，寫入資料庫----------------
 		try (InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "product_detail.dat"), "UTF-8");
-			 BufferedReader br = new BufferedReader(isr0);)
-		{
+				BufferedReader br = new BufferedReader(isr0);) {
 			while ((line = br.readLine()) != null) {
 				// 未處理BOM字元，若有需要，請自行加入
 				String[] sa = line.split("\\|");
@@ -265,8 +329,7 @@ public class EDMTableResetHibernateGBG {
 
 		// -------------讀取product_pic資料，寫入資料庫----------------
 		try (InputStreamReader isr0 = new InputStreamReader(new FileInputStream(path + "product_pic.dat"), "UTF-8");
-			 BufferedReader br = new BufferedReader(isr0);)
-		{
+				BufferedReader br = new BufferedReader(isr0);) {
 			while ((line = br.readLine()) != null) {
 				// 未處理BOM字元，若有需要，請自行加入
 				String[] sa = line.split("\\|");
